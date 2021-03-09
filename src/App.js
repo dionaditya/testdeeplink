a6ade5c0203c66429574d10fe9c2bd2963ca6e41
Dion Aditya <dionjatingaleh@gmail.com>
2021-03-09 17:37:39 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -26,6 +26,12 @@ const handleOpenInApp = () => {
     }
 
   }, 1)
+  setTimeout(() => {
+    let url;
+    if (android) { url = 'https://play.google.com/store/apps/details?id=id.co.duniagames'; }
+    if (ios) { url = 'https://apps.apple.com/id/app/dunia-games/id1446034036?l=id'; }
+    window.location.href = url;
+  }, 100);
 
 }
 
